fac1a49b3b798eda1209d79b3feb53427c8f27f7
lexijw <alexis@noblebythesea.com>
2025-07-25 19:01:22 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Banner from "../src/components/Banner.vue";
+import SidebarMenu from "../src/components/SidebarMenu.vue";
+import Footer from "../src/components/Footer.vue";
+</script>
+
+<template>
+  <Banner
+    :announcements="[
+      'Happy hour starts at 3:00pm everyday! Enjoy discounted cocktails and draft beer!',
+      'Check our social media for live music updates!',
+      'Menu offerings can change each day. Come and see if we have your favourite, or try something new!',
+    ]"
+  />
+  <SidebarMenu />
+  <RouterView />
+  <Footer />
+</template>
+
+<style scoped></style>
---
+++ src/views/AboutPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "../components/AboutHero.vue";
+import AboutSection from "../components/AboutSection.vue";
+</script>
+
+<template>
+  <Hero></Hero>
+  <AboutSection></AboutSection>
+</template>
+
+<style scoped></style>
