cccd2c6479c9b81c02565136b51acd58bd66c11d
lexijw <alexis@noblebythesea.com>
2025-07-25 20:07:16 -0300

    Added Netlify plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), netlify()],
 });
